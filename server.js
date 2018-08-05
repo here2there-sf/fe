8b4544cac7d15f9f5e76dd248ed1914ebfccbb6d
benkrig <ben@sooryen.com>
2018-08-05 13:31:40 -0700

    heroku ang build

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist/fe'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 4200, function() {
+  console.log('Listening on port ' + this.address().port); //Listening on port 8888
+});
