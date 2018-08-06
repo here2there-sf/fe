266fcdb078f0e4c30abf75f3237b8c7020645b16
benkrig <benjaminjkrig@gmail.com>
2018-08-06 14:30:07 -0700

    fix 404 issue.

--- server.js
+++ server.js
@@ -11,3 +11,7 @@ app.use(express.static(__dirname + '/dist/fe'));
 app.listen(process.env.PORT || 4200, function() {
   console.log('Listening on port ' + this.address().port); //Listening on port 8888
 });
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist/fe', 'index.html'));
+});
